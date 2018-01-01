b74abdc900cdd2e88f97bf2a963819fd428aee0a
zhangxiaokang <kangeneral@126.com>
2018-01-01 22:16:33 +0800

    初始化

--- src/main/java/com/zhang/picpig/PigpicApplication.java
+++ src/main/java/com/zhang/picpig/PigpicApplication.java
@@ -1,4 +1,15 @@
 package com.zhang.picpig;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zxk
+ */
+@SpringBootApplication
 public class PigpicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PigpicApplication.class, args);
+    }
 }
